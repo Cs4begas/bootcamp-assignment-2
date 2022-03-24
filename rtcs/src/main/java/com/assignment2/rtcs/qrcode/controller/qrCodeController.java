5c22dd51167a54dcd76cc54592e85cf476072749
nattapan <formulae445@gmail.com>
2022-03-24 19:22:07 +0700

    #1 Initial project

---
+++ rtcs/src/main/java/com/assignment2/rtcs/qrcode/controller/qrCodeController.java
@@ -0,0 +1,17 @@
+package com.assignment2.rtcs.qrcode.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Optional;
+
+public class qrCodeController {
+
+//    @PostMapping("/api/qrcode")
+//    @ResponseBody
+//    public Basket CreateBasket(@RequestHeader("customerId") Optional<Integer> customerId, @RequestBody(required = false) CreateBasketRequest basketRequest) {
+//        return basketBusinessLogic.CreateBasket(customerId, basketRequest);
+//    }
+}
